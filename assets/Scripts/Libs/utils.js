5e0a6eaf02d2aeb947a23f403d9056a9a78cac88
wheatup <acphun@gmail.com>
2019-03-12 10:42:33 +0900

    new utils function

--- assets/Scripts/Libs/utils.js
+++ assets/Scripts/Libs/utils.js
@@ -27,6 +27,45 @@ class Utils {
 	static distance(vec1, vec2) {
 		return Math.sqrt(Math.pow(vec1.x - vec2.x, 2) + Math.pow(vec1.y - vec2.y, 2));
 	}
+
+	/**
+	 * Switch the parent of the node without changing the transformation
+	 * @param node target node
+	 * @param newParent 
+	 */
+	static switchParent(node, newParent) {
+		let localPos = (node.parent || node).convertToWorldSpaceAR(node.getPosition());
+		let worldPos = newParent.convertToNodeSpaceAR(localPos);
+
+		let rotationAttr = (typeof node.rotation === 'undefined' ? 'angle' : 'rotation');
+		let localScaleX = 1;
+		let localScaleY = 1;
+		let localAngleX = 0;
+		let localAngleY = 0;
+		for (let parent = node; parent; parent = parent.parent) {
+			localScaleX *= parent.scaleX;
+			localScaleY *= parent.scaleY;
+			localAngleX += parent[rotationAttr + 'X'];
+			localAngleY += parent[rotationAttr + 'Y'];
+		}
+
+		let worldScaleX = 1;
+		let worldScaleY = 1;
+		let worldAngleX = 0;
+		let worldAngleY = 0;
+		for (let parent = newParent; parent; parent = parent.parent) {
+			worldScaleX *= parent.scaleX;
+			worldScaleY *= parent.scaleY;
+			worldAngleX += parent[rotationAttr + 'X'];
+			worldAngleY += parent[rotationAttr + 'Y'];
+		}
+
+		node.setParent(newParent);
+		node[rotationAttr+'X'] = localAngleX + worldAngleX;
+		node[rotationAttr+'Y'] = localAngleY + worldAngleY;
+		node.setScale(localScaleX * worldScaleX, localScaleY * worldScaleY);
+		node.setPosition(worldPos);
+	}
 }
 
 window.Utils = Utils;
